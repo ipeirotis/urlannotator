5bc983e7f80cefad79928380ff8661875047347d
Przemysław Spodymek <madvril@gmail.com>
2012-07-13 14:53:57 +0200

    OANNOTATOR-37 & OANNOTATOR-38 Capturing screens & retrieving web text dump. No error checking and fallback for now.

--- urlannotator/main/tasks.py
+++ urlannotator/main/tasks.py
@@ -1,10 +1,17 @@
+import os
 import subprocess
 
 from django.conf import settings
+from django.template.defaultfilters import slugify
 
 from celery import task
 from boto.s3.connection import S3Connection
 from boto.s3.key import Key
+
+from urlannotator.main.models import Sample
+
+SCREEN_DUMPS_BUCKET_NAME = "urlannotator_web_screenshot"
+S3_SERVER_NAME = "https://s3.amazonaws.com/"
 
 
 @task()
@@ -13,34 +20,45 @@ def add(x, y):
 
 
 @task()
-def web_content_extraction(url):
+def web_content_extraction(sample_id, url=None):
     """ Links/lynx required. Generates html output from those browsers.
     """
+    if url is None:
+        url = Sample.objects.get(id=sample_id).url
+
     text = subprocess.check_output(["links", "-dump", url])
-
-    conn = S3Connection(settings.AWS_ACCESS_KEY_ID,
-        settings.AWS_SECRET_ACCESS_KEY)
-    bucket = conn.create_bucket('web_content')
-    k = Key(bucket)
-    k.key = url
-
-    k.set_contents_from_string(text)
+    Sample.objects.get(id=sample_id).update(text=text)
 
     return True
 
 
 @task()
-def web_screenshot_extraction(url):
+def web_screenshot_extraction(sample_id, url=None):
     """ CutyCapt required. Generates html output from those browsers.
     """
-    # text = subprocess.check_output(["links", "-dump", url])
+    if url is None:
+        url = Sample.objects.get(id=sample_id).url
 
-    # conn = S3Connection(settings.AWS_ACCESS_KEY_ID,
-    #     settings.AWS_SECRET_ACCESS_KEY)
-    # bucket = conn.create_bucket('web_content')
-    # k = Key(bucket)
-    # k.key = url
+    slugified_url = slugify(url)
+    screen_dir = "urlannotator_web_screenshot"
+    screen_out = "%s/%s.png" % (screen_dir, slugified_url)
 
-    # k.set_contents_from_string(text)
+    os.system("mkdir %s" % screen_dir)
+    os.system('cutycapt --url="%s" --out="%s"' % (url, screen_out))
 
-    # return True
+    conn = S3Connection(settings.AWS_ACCESS_KEY_ID,
+        settings.AWS_SECRET_ACCESS_KEY)
+    bucket = conn.create_bucket(SCREEN_DUMPS_BUCKET_NAME)
+    k = Key(bucket)
+    k.key = url
+    k.set_contents_from_filename(screen_out)
+
+    # Give access to view screen.
+    k.make_public()
+
+    # Url for public screen (without any expiration date)
+    screenshot_url = S3_SERVER_NAME + SCREEN_DUMPS_BUCKET_NAME + '/' + k.name
+
+    Sample.objects.get(id=sample_id).update(screenshot=screenshot_url)
+
+    return True
