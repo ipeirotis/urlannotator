d855d1a676456d4c18308490c4a818e72321e0ee
Przemysław Spodymek <madvril@gmail.com>
2012-07-16 16:50:25 +0200

    Settings for local broker - RabbitMQ. SQS used on production only.

--- urlannotator/settings/defaults.py
+++ urlannotator/settings/defaults.py
@@ -154,6 +154,7 @@ BASE_APPS = (
 
 PROJECT_APPS = (
     'urlannotator.main',
+    'urlannotator.tools',
     'urlannotator.classification',
     'urlannotator.crowdsourcing',
 )
@@ -272,23 +273,4 @@ CELERY_IMPORTS = (
 )
 
 # Broker for celery
-# WARN: Do not use the amqp backend with SQS.
-BROKER_URL = 'sqs://%s:%s@' % (AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY)
-
-# Do we need this config?
-BROKER_TRANSPORT_OPTIONS = {
-    'region': 'eu-west-1',
-    # 'visibility_timeout': 3600,
-    # 'polling_interval': 0.3,
-    'queue_name_prefix': 'urlannotator-',
-}
-
-# Celery database backend
-# By default celery stores state in django db.
-# CELERY_RESULT_BACKEND = "database"
-# CELERY_RESULT_DBURI = "sqlite:///celerydb.sqlite"
-# CELERY_RESULT_DBURI = "mysql://scott:tiger@localhost/foo"
-
-# We can ignore results from celery tasks (no need for db backend)
-# We can also do this by: @celery.task(ignore_result=True)
-# CELERY_IGNORE_RESULT = True
+BROKER_URL = 'amqp://guest:guest@localhost:5672//'
