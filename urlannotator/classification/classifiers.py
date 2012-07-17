56530bd3480cca7960e05849224c23c4a2972183
Przemysław Spodymek <madvril@gmail.com>
2012-07-17 11:41:36 +0200

    OANNOTATOR-62 Switch options in 247 classifier. Some docstrings.

--- urlannotator/classification/classifiers.py
+++ urlannotator/classification/classifiers.py
@@ -10,20 +10,43 @@ class Classifier247(object):
 
     def __init__(self, classifier_cls, rwlock_cls=MemcachedRWLock,
             lock_cls=MemcacheLock):
+        """
+        Our permanent (24/7) classifier can be initialized with custom
+        classifier class - classifier_cls (which performs real classification) &
+        with custom locking implementation. On default we use MamcacheLock and
+        MemcachedRWLock.
+        """
+
         self.lock = lock_cls()
         self.rwlock = rwlock_cls()
         self.read_classifier = classifier_cls()
         self.write_classifier = classifier_cls()
 
     def train(self, *args, **kwargs):
+        """
+        Writing task. Lock write classifier.
+        On default after training switch is performed. This can be disabled by
+        passing switch=False in kwargs.
+        """
+
+        # Switch can be disabled.
+        switch = True
+        if 'switch' in kwargs:
+            switch = kwargs['switch']
+            kwargs.pop('switch')
+
         self.lock.acquire()
         result = self.write_classifier.train(*args, **kwargs)
-        self.switch()
+        if switch:
+            self._switch_with_lock()
         self.lock.release()
 
         return result
 
     def update(self, *args, **kwargs):
+        """ Writing task. Lock write classifier.
+        """
+
         self.lock.acquire()
         result = self.write_classifier.update(*args, **kwargs)
         self.lock.release()
@@ -31,6 +54,9 @@ class Classifier247(object):
         return result
 
     def classify(self, *args, **kwargs):
+        """ Reading task. Read classifier is used - aquire reader rwlock.
+        """
+
         self.rwlock.reader_acquire()
         result = self.read_classifier.classify(*args, **kwargs)
         self.rwlock.reader_release()
@@ -38,6 +64,9 @@ class Classifier247(object):
         return result
 
     def classify_with_info(self, *args, **kwargs):
+        """ Reading task. Read classifier is used - aquire reader rwlock.
+        """
+
         self.rwlock.reader_acquire()
         result = self.read_classifier.classify_with_info(*args, **kwargs)
         self.rwlock.reader_release()
@@ -45,6 +74,21 @@ class Classifier247(object):
         return result
 
     def switch(self):
+        """
+        Cold switch. Aquires all locks and runs switch. In result whole
+        classifier is blocked for switch time.
+        """
+
+        self.lock.acquire()
+        self._switch_with_lock()
+        self.lock.release()
+
+    def _switch_with_lock(self):
+        """
+        Hot switch. Use only when ensured that write classifier is not used.
+        F.e. when training task ends.
+        """
+
         self.rwlock.writer_acquire()
         (self.read_classifier, self.write_classifier) = (self.write_classifier,
             self.read_classifier)
