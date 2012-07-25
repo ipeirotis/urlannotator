9de70a0d4e5ee52b321399c102ed3667ef8ba42f
maciej-gol <maciej.gol@10clouds.com>
2012-07-25 09:39:11 +0200

    Throttled sample gathering speed to a maximum of 20 samples every 2 minutes.

--- urlannotator/sample_gathering/simple_gatherer.py
+++ urlannotator/sample_gathering/simple_gatherer.py
@@ -11,7 +11,10 @@ from urlannotator.main.models import Job, Worker
 from urlannotator.flow_control import send_event
 
 # Number of seconds between each gather
-TIME_INTERVAL = 30
+TIME_INTERVAL = 2 * 60
+
+# Maximum number of samples collected per run
+SAMPLE_LIMIT = 20
 
 
 class SimpleGatherer(object):
@@ -28,12 +31,19 @@ class SimpleGatherer(object):
                 'http://www.dec.ny.gov/animals/9358.html',
                 'http://www.enchantedlearning.com/subjects/mammals/raccoon/Raccoonprintout.shtml']
             i = 0
+            to_collect = SAMPLE_LIMIT
             for job in jobs:
-                for j in xrange(job.remaining_urls):
+                if to_collect == 0:
+                    break
+
+                collected = 0
+                for j in xrange(min(job.remaining_urls, to_collect)):
                     send_event('EventNewRawSample', job.id, w.id, urls[i])
+                    collected += 1
                     i = (i + 1) % len(urls)
-                job.remaining_urls = 0
+                job.remaining_urls -= collected
                 job.save()
+                to_collect -= collected
             time.sleep(TIME_INTERVAL)
 
 SimpleGatherer().run()
