cb154d297e2bc02857d582d755aca163a361c1f7
maciej-gol <maciej.gol@10clouds.com>
2012-07-31 09:47:36 +0200

    Remove redundant code

--- urlannotator/main/backends/pipeline.py
+++ urlannotator/main/backends/pipeline.py
@@ -20,7 +20,6 @@ def create_user(request, user, details, *args, **kwargs):
             % (request.session['registration'], details['username'])
         user = User.objects.create_user(username=username, email=email,
             password='!')
-        print 'new user'
         is_new = True
         request.session.pop('registration')
     elif user is None:
