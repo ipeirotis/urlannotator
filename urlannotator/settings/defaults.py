c2f4ad58d8271e632eb1e625900b23f9b12fc12e
maciej-gol <maciej.gol@10clouds.com>
2012-08-13 15:41:31 +0200

    Fix celery memory limit script. Add testing flag to defaults.py

--- urlannotator/settings/defaults.py
+++ urlannotator/settings/defaults.py
@@ -312,3 +312,6 @@ TEST_RUNNER = 'djcelery.contrib.test_runner.CeleryTestSuiteRunner'
 TAGASAURIS_LOGIN = ''
 TAGASAURIS_PASS = ''
 TAGASAURIS_HOST = ''
+
+# Tools testing flag. If set to False, several tool's behaviour won't be mocked
+TOOLS_TESTING = False
