5694629d3260a1144a6e633893ac9f37144793e0
maciej-gol <maciej.gol@10clouds.com>
2012-08-14 11:43:28 +0200

    Make test db in file, not memory.

--- urlannotator/settings/development.py
+++ urlannotator/settings/development.py
@@ -7,6 +7,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(ROOT_DIR, '..', 'database.sqlite3.db'),
+        'TEST_NAME': os.path.join(ROOT_DIR, '..', 'test_database.sqlite3.db'),
     }
 }
 
