eb34269ac8dc9fe38452ca57063511e369520381
maciej-gol <maciej.gol@10clouds.com>
2012-09-04 14:25:16 +0200

    Corrections to rabbitmq script

--- deployment/scripts/rabbitmq.sh
+++ deployment/scripts/rabbitmq.sh
@@ -3,9 +3,9 @@
 . %(virtualenv_dir)s/bin/activate
 
 rabbitmqctl -n %(project_name)s status
-if [ $? != 0]
+if [ $? != 0 ]
 then
-    exec rabbitmq_server
+    exec rabbitmq-server
 else
     rabbitmqctl -n %(project_name)s stop_app
     rabbitmqctl -n %(project_name)s reset
