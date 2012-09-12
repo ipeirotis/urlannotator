08f8eae9bd4ad73bfc7cb266334a1c0b0c90bd67
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-09-12 11:57:01 +0200

    Added API definition for quality algorithms

--- urlannotator/crowdsourcing/quality/algorithms.py
+++ urlannotator/crowdsourcing/quality/algorithms.py
@@ -1,6 +1,32 @@
 from collections import Counter
 
 
+class CrowdsourcingQualityAlgorithm(object):
+
+    def __init__(self, job_id):
+        ''' That job id will be needed in some cases - like in Troia
+        '''
+        self.job_id = job_id
+
+    def add_vote(self, worker_id, object_id, label):
+        ''' adds vote from given worker that was assigned to given object
+        '''
+
+    def add_votes(self, votes):
+        ''' Default implementation '''
+        for worker_id, object_id, label in votes:
+            self.add_vote(worker_id, object_id, label)
+
+    def reset(self):
+        ''' Clears all stored votes and helper data
+        '''
+
+    def extract_decisions(self):
+        ''' Should return predicted labels for objects in form:
+            [(object_id, label), (object_id, label), ...]
+        '''
+
+
 class MajorityVoting(object):
     """
     Simple majority voting algorithm.
