3d220f6319273bcbca144b116ed5f06dc5c9c55c
Konrad Kurdej <konrad.kurdej@10clouds.com>
2012-09-13 16:35:51 +0200

    Update in script - more general, no filter on person

--- what_left.sh
+++ what_left.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 gu=`git config user.name`
+gu=""
 ESCAPES="TODO|XXX|FIXME"
 for f in `find urlannotator/ -type f | grep "\.py$" | grep -v "/migrations/"`; do 
     r=`git blame $f | grep "$gu" | egrep "$ESCAPES"`
     if [ -n "$r" ]; then
-        echo "In file $f :"
+        echo "$f"
         echo -e "$r"
         echo
     fi
