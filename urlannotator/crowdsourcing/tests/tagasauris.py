ced87f47c0f67ff36a2210d15420e8d7d5be25c0
Przemysław Spodymek <madvril@gmail.com>
2012-09-20 16:21:07 +0200

    OANNOTATOR-208 Sample voting manager test.

--- urlannotator/crowdsourcing/tests/tagasauris.py
+++ urlannotator/crowdsourcing/tests/tagasauris.py
@@ -2,10 +2,14 @@ from django.test import TestCase
 from django.conf import settings
 from django.contrib.auth.models import User
 
+from celery import task
+
 from urlannotator.main.models import Job, Sample
 from urlannotator.crowdsourcing.tagasauris_helper import (make_tagapi_client,
     create_job, sample_to_mediaobject)
+from urlannotator.crowdsourcing.models import SampleMapping, TagasaurisJobs
 from urlannotator.flow_control.test import ToolsMockedMixin
+from urlannotator.flow_control import send_event
 
 
 class TagasaurisHelperTest(ToolsMockedMixin, TestCase):
@@ -34,3 +38,52 @@ class TagasaurisHelperTest(ToolsMockedMixin, TestCase):
         self.assertEqual(len(mo['id']), 32)
         self.assertEqual(mo['url'], self.sample.screenshot)
         self.assertEqual(mo['mimetype'], 'image/png')
+
+
+@task()
+def mocked_task(*args, **kwargs):
+    return "http://www.10clouds.com/media/v1334047194.07/10c/images/10c_logo.png"
+
+
+class TagasaurisSampleVotingTest(ToolsMockedMixin, TestCase):
+
+    mocks = [
+        ('urlannotator.main.factories.web_content_extraction', mocked_task),
+    ]
+
+    def setUp(self):
+        self.u = User.objects.create_user(username='testing', password='test')
+        self.job = Job.objects.create_active(
+            title='test_tagapi_client',
+            description='test_description',
+            no_of_urls=2,
+            account=self.u.get_profile(),
+            gold_samples=[
+                {'url': '10clouds.com/1', 'label': 'Yes'},
+                {'url': '10clouds.com/2', 'label': 'Yes'},
+                {'url': '10clouds.com/3', 'label': 'Yes'}
+            ]
+        )
+
+        TagasaurisJobs.objects.create(urlannotator_job=self.job)
+
+        for s in Sample.objects.all():
+            s.screenshot = "http://www.10clouds.com/media/v1334047194.07/10c/images/10c_logo.png"
+            s.save()
+
+    def testEventSamplesVoting(self):
+        self.assertEqual(TagasaurisJobs.objects.count(), 1)
+        self.assertEqual(TagasaurisJobs.objects.all()[0].voting_key, None)
+        self.assertEqual(TagasaurisJobs.objects.all()[0].voting_hit, None)
+
+        send_event('EventSamplesVoting')
+
+        self.assertEqual(SampleMapping.objects.count(), 3)
+
+        self.assertEqual(SampleMapping.objects.all()[0].crowscourcing_type,
+            SampleMapping.TAGASAURIS)
+        self.assertEqual(len(SampleMapping.objects.all()[0].external_id), 32)
+
+        self.assertEqual(TagasaurisJobs.objects.count(), 1)
+        self.assertEqual(len(TagasaurisJobs.objects.all()[0].voting_key), 32)
+        self.assertEqual(len(TagasaurisJobs.objects.all()[0].voting_hit), 32)
