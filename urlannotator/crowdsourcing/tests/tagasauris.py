af89583594d5b6c45d0b99e43581cb5f010c0ae2
Przemysław Spodymek <madvril@gmail.com>
2012-09-21 12:39:02 +0200

    OANNOTATOR-208 Test for distinct HID generation.

--- urlannotator/crowdsourcing/tests/tagasauris.py
+++ urlannotator/crowdsourcing/tests/tagasauris.py
@@ -32,6 +32,14 @@ class TagasaurisHelperTest(ToolsMockedMixin, TestCase):
         self.assertEqual(len(voting_hit), 32)
         self.assertEqual(len(voting_key), 32)
 
+    def testJobDistinctHID(self):
+        voting_key_1, voting_hit_1 = create_job(self.tc, self.job,
+            settings.TAGASAURIS_SAMPLE_GATHERER_WORKFLOW)
+        voting_key_2, voting_hit_2 = create_job(self.tc, self.job,
+            settings.TAGASAURIS_SAMPLE_GATHERER_WORKFLOW)
+        self.assertNotEqual(voting_key_1, voting_key_2)
+        self.assertNotEqual(voting_hit_1, voting_hit_2)
+
     def testSampleConvertion(self):
         self.sample.screenshot = 'test_123'
         mo = sample_to_mediaobject(self.sample)
