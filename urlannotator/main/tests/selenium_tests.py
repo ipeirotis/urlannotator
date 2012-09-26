b83a36d1ccddcefa3bde0be536a2708f9eb1d085
Piotr Frankowski <piotr.frankowski@10clouds.com>
2012-09-26 10:02:58 +0200

    fix sellenium tests - landing page cause fail

--- urlannotator/main/tests/selenium_tests.py
+++ urlannotator/main/tests/selenium_tests.py
@@ -39,8 +39,7 @@ class RegistrationSeleniumTests(LiveServerTestCase):
 
         self.selenium.get('%s%s'
                           % (self.live_server_url, reverse('logout')))
-        selector = '//ul[@class="nav pull-right loginNav"]//li[@class="dropdown"]'\
-        '/a[@class="dropdown-toggle"]'
+        selector = '//a[@class="btn pull-right btn-login"]'
         el = self.selenium.find_element_by_xpath(selector)
         self.assertTrue(el)
 
