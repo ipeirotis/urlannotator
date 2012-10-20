3f75c302a6bb81bef82ba88ce3b59d78122dd1dd
maciej-gol <maciej.gol@10clouds.com>
2012-10-20 09:13:31 +0200

    Unintended changes reverted.

--- urlannotator/settings/defaults.py
+++ urlannotator/settings/defaults.py
@@ -41,7 +41,7 @@ TWENTYFOUR_DEFAULT_CLASSIFIER = 'GooglePredictionClassifier'
 VOTES_STORAGE = 'TroiaVotesStorage'
 QUALITY_ALGORITHM = 'DawidSkeneAlgorithm'
 
-SITE_URL = '89.76.71.95:30000'
+SITE_URL = 'devel.urlannotator.10clouds.com'
 
 SOCIAL_AUTH_CREATE_USERS = False
 
